193fc8fd078e56d01c0b4e7a0275df3edd875aaa
scott-feng <909698516@qq.com>
2015-12-13 19:49:30 +0800

    hannuota

---
+++ Hanoi.c
@@ -0,0 +1,24 @@
+#include"stdio.h"
+#define N 100
+int main()
+{
+	int a;
+    int i=0;
+	int sum=0;
+   while(1)
+   {
+       scanf("%d\n",&a);
+       if(a==0)
+               break;
+   }
+   a=0;
+    while(a!=0)
+	{
+		sum+=a;
+        i++;
+	}
+
+	printf("the result is %d\n",sum);
+       return 0;	
+
+}	
